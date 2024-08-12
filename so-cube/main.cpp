705bcaec173f44332fec4c4a898b42e09b17c36f
Roman Shulgan <shulgan.r@niuitmo.ru>
2024-08-12 19:55:55 +0400

    commit_#1

--- so-cube/main.cpp
+++ so-cube/main.cpp
@@ -12,11 +12,33 @@ float Oy = window.getSize().y % 2 == 0 ? float(window.getSize().y + 1) / 2 : flo
 
 class Polygon {
 public:
-    explicit Polygon(float d) : depth(d) {
+    explicit Polygon(float d) : depth(d), displacement(0, 0, 0) {
+        for(auto i: vec) {
+            i.first = {0, 0, 0};
+            i.second = Color::Transparent;
+        }
+    }
+
+    void move(Vector3f v, float alpha, float beta, float gamma) {
+        unprojection();
+        for (auto &i: vec) {
+            i.first -= displacement;
+
+            i.first = {cos(beta) * cos(gamma) * i.first.x - sin(gamma) * cos(beta) * i.first.y + sin(beta) * i.first.z,
+                       sin(alpha) * sin(beta) * cos(gamma) * i.first.x + sin(gamma) * cos(alpha) * i.first.x -
+                       sin(alpha) * sin(beta) * sin(gamma) * i.first.y + cos(alpha) * cos(gamma) * i.first.y -
+                       sin(alpha) * cos(beta) * i.first.z,
+                       sin(alpha) * sin(gamma) * i.first.x - sin(beta) * cos(alpha) * cos(gamma) * i.first.x +
+                       sin(alpha) * cos(gamma) * i.first.y + sin(beta) * sin(gamma) * cos(alpha) * i.first.y +
+                       cos(alpha) * cos(beta) * i.first.z};
+
+            i.first += (displacement + v);
+        }
+        displacement += v;
         projection();
     }
 
-    void update(vector<pair<Vector3f, Color>> v) {
+    void setUp(vector<pair<Vector3f, Color>> v) {
         this->vec = std::move(v);
         projection();
     }
@@ -46,6 +68,7 @@ public:
     }
 private:
     vector<pair<sf::Vector3f, Color>> vec;
+    Vector3f displacement;
     float depth;
 
     [[nodiscard]] Vector3f center(const vector<pair<Vector3f, Color>>& v) const {
@@ -75,105 +98,86 @@ private:
             i.first.y *= depth / i.first.z;
         }
     }
+
+    void unprojection() {
+        for(pair<Vector3f, Color> & i : vec) {
+            i.first.x /= depth / i.first.z;
+            i.first.y /= depth / i.first.z;
+        }
+    }
 };
 
 float a = 130;
-
-Vector3f vec_x = {a, 0, 0};
-Vector3f vec_y = {0, a, 0};
-Vector3f vec_z = {0, 0, a};
-
-Vector3f v_xyz, v0, v1, v2, v3, v4, v5, v6, v7, v8;
-
-vector<vector<pair<Vector3f, Color>>> v(6);
-vector<Polygon> vp;
+Vector3f v0(a, a, a);
+Vector3f v1(-a, a, a);
+Vector3f v2(a, -a, a);
+Vector3f v3(-a, -a, a);
+Vector3f v4(a, a, -a);
+Vector3f v5(-a, a, -a);
+Vector3f v6(a, -a, -a);
+Vector3f v7(-a, -a, -a);
 
 //vector<Polygon> white_side;
 //vector<vector<pair<Vector3f, Color>>> ws(9);
-
-void rot_space(Vector3f& vec, float alpha, float beta, float gamma) {
-    vec = {cos(beta) * cos(gamma) * vec.x - sin(gamma) * cos(beta) * vec.y + sin(beta) * vec.z,
-           sin(alpha) * sin(beta) * cos(gamma) * vec.x + sin(gamma) * cos(alpha) * vec.x -
-           sin(alpha) * sin(beta) * sin(gamma) * vec.y + cos(alpha) * cos(gamma) * vec.y -
-           sin(alpha) * cos(beta) * vec.z,
-           sin(alpha) * sin(gamma) * vec.x - sin(beta) * cos(alpha) * cos(gamma) * vec.x +
-           sin(alpha) * cos(gamma) * vec.y + sin(beta) * sin(gamma) * cos(alpha) * vec.y +
-           cos(alpha) * cos(beta) * vec.z};
-}
-
-
-
-void update(float x = 0, float y = 0, float z = 0, float alpha = 0, float beta = 0, float gamma = 0) {
-    v_xyz += {x, y, z};
-
-    rot_space(vec_x, alpha, beta, gamma);
-    rot_space(vec_y, alpha, beta, gamma);
-    rot_space(vec_z, alpha, beta, gamma);
-
-    v1 = vec_z + vec_y - vec_x + v_xyz,
-    v2 = vec_z + vec_y + vec_x + v_xyz,
-    v3 = vec_z - vec_y - vec_x + v_xyz,
-    v4 = vec_z - vec_y + vec_x + v_xyz,
-    v5 = vec_x - vec_y - vec_z + v_xyz,
-    v6 = -vec_x - vec_y - vec_z + v_xyz,
-    v7 = vec_x + vec_y - vec_z + v_xyz,
-    v8 = -vec_x + vec_y - vec_z + v_xyz;
-
-    v[0][0].first = v1;
-    v[0][1].first = v2;
-    v[0][2].first = v3;
-    v[0][3].first = v4;
-
-    v[1][0].first = v1;
-    v[1][1].first = v3;
-    v[1][2].first = v8;
-    v[1][3].first = v6;
-
-    v[2][0].first = v2;
-    v[2][1].first = v4;
-    v[2][2].first = v7;
-    v[2][3].first = v5;
-
-    v[3][0].first = v5;
-    v[3][1].first = v6;
-    v[3][2].first = v7;
-    v[3][3].first = v8;
-
-    v[4][0].first = v1;
-    v[4][1].first = v2;
-    v[4][2].first = v8;
-    v[4][3].first = v7;
-
-    v[5][0].first = v3;
-    v[5][1].first = v4;
-    v[5][2].first = v6;
-    v[5][3].first = v5;
-
-    for(int i = 0; i < v.size(); i++)
-        vp[i].update(v[i]);
-}
 
 bool compare(const Polygon& left,  const Polygon& right) {
     return left > right;
 }
 
+void rotate(vector<Polygon>& v, float delta);
+
 int main() {
+    vector<vector<pair<Vector3f, Color>>> v(6);
+    vector<Polygon> vp;
     float x, y, z, alpha, beta, gamma;
 
     for (int i = 0; i < v.size(); i++)
         vp.emplace_back(600);
 
-    vector<Color> colors = {Color(255, 165, 0, 255), Color::Blue, Color::Green, Color::Red, Color::Yellow,
-                            Color::White};
+    vector<Color> colors = {Color::Yellow, Color::White, Color::Blue, Color::Green, Color(255, 165, 0, 255),
+                            Color::Red};
 
     for (int i = 0; i < 6; i++)
         for (int j = 0; j < 4; j++)
             v[i].emplace_back(Vector3f(), colors[i]);
 
-    update(0, 0, 600, -M_PI / 10, M_PI / 4, M_PI);
+    v[0][0].first = v0;
+    v[0][1].first = v1;
+    v[0][2].first = v2;
+    v[0][3].first = v3;
 
-    for (int i = 0; i < v.size(); i++)
-        vp[i].update(v[i]);
+    v[1][0].first = v4;
+    v[1][1].first = v5;
+    v[1][2].first = v6;
+    v[1][3].first = v7;
+
+    v[2][0].first = v0;
+    v[2][1].first = v2;
+    v[2][2].first = v4;
+    v[2][3].first = v6;
+
+    v[3][0].first = v1;
+    v[3][1].first = v3;
+    v[3][2].first = v5;
+    v[3][3].first = v7;
+
+    v[4][0].first = v0;
+    v[4][1].first = v1;
+    v[4][2].first = v4;
+    v[4][3].first = v5;
+
+    v[5][0].first = v2;
+    v[5][1].first = v3;
+    v[5][2].first = v6;
+    v[5][3].first = v7;
+
+    for (int i = 0; i < v.size(); i++) {
+        vp[i].setUp(v[i]);
+        vp[i].move({0, 0, 600}, M_PI / 2, 0, 0);
+        vp[i].move({0, 0, 0}, 0, M_PI / 4, 0);
+        vp[i].move({0, 0, 0}, -M_PI / 8, 0, 0);
+    }
+
 
     while (window.isOpen()) {
         Event event{};
@@ -184,10 +188,12 @@ int main() {
             if (event.type == sf::Event::KeyPressed) {
                 x = y = z = alpha = beta = gamma = 0;
 
-                if (event.key.code == sf::Keyboard::S) y += -50;
-                if (event.key.code == sf::Keyboard::W) y += 50;
-                if (event.key.code == sf::Keyboard::A) x += -50;
-                if (event.key.code == sf::Keyboard::D) x += 50;
+                if (event.key.code == sf::Keyboard::S) y = -50;
+                if (event.key.code == sf::Keyboard::W) y = 50;
+                if (event.key.code == sf::Keyboard::A) x = -50;
+                if (event.key.code == sf::Keyboard::D) x = 50;
+                if (event.key.code == sf::Keyboard::Q) z = -50;
+                if (event.key.code == sf::Keyboard::E) z = 50;
                 if (event.key.code == sf::Keyboard::L) alpha = -M_PI / 60;
                 if (event.key.code == sf::Keyboard::J) alpha = M_PI / 60;
                 if (event.key.code == sf::Keyboard::O) beta = -M_PI / 60;
@@ -195,13 +201,25 @@ int main() {
                 if (event.key.code == sf::Keyboard::I) gamma = -M_PI / 60;
                 if (event.key.code == sf::Keyboard::K) gamma = M_PI / 60;
 
-                update(x, y, z, alpha, beta, gamma);
+                for(auto &i: vp)
+                    i.move({x, y, z}, alpha, beta, gamma);
             }
         }
 
         window.clear();
 
+        for(auto& i: vp) {
+            i.move({0, 0, 0}, M_PI / 8, 0, 0);
+            i.move({0, 0, 0}, 0, -M_PI / 4, 0);
+
+            i.move({0, 0, 0}, 0, -M_PI / (360 * 50), 0);
+
+            i.move({0, 0, 0}, 0, M_PI / 4, 0);
+            i.move({0, 0, 0}, -M_PI / 8, 0, 0);
+        }
+
         std::sort(begin(vp), end(vp), compare);
+
         for (auto i: vp)
             window.draw(&i.getVer()[0], i.size(), TriangleStrip);
 
